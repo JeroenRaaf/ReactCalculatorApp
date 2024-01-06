ca20ecfc3974e43f5f9d995f7cfcfa3ea0d94f17
JeroenRaaf <jeroenraaf@hotmail.com>
2024-01-06 16:32:56 +0100

    Version 1. I can always add more features if I feel like it one day.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ReactCalculatorApp
 Made this while bored, but enjoy.
+
+If I want to, I can someday add:
+- Decimal numbers. The button for decimal (,) is already in the code, but i decided that I would keep it for later.
+- Calculating using a previously calculated number.
--- app/src/App.js
+++ app/src/App.js
@@ -2,46 +2,93 @@ import './App.css';
 import {useEffect, useState} from "react";
 
 function App() {
-    const [screen, setScreen] = useState(0)
+    const [screen, setScreen] = useState("")
+    const [errormsg, setErrormsg] = useState("")
+    const [result, setResult] = useState(0)
 
     useEffect(() => {
-        setScreen(35453);
+        setScreen("0");
     }, []);
 
-    const handleButttonClick = (e) => {
-        console.log(e.target.value)
+    const handleButtonClick = (e) => {
+
+        setErrormsg("");
+        let error = false;
+
+        if (screen === "0" && isNaN(e) || screen === result.toString() && isNaN(e)) {
+            error = true;
+            setErrormsg("You can't start with an operator.")
+        } else if (isNaN(e) && isNaN(screen[screen.length - 1])) {
+            error = true;
+            setErrormsg("You can't use two operators in a row.");
+        }
+
+        if(!error) {
+            if(screen === "0" || screen === result.toString()) {
+                setScreen(e.toString());
+            } else{
+                setScreen(screen + e.toString());
+            }
+        }
+    }
+
+    const handleCalcClick = (e) => {
+        if(screen === "0") {
+            setErrormsg("You can't calculate with only 0.");
+        } else if(isNaN(screen[screen.length - 1])) {
+            setErrormsg("You can't end with an operator.");
+        } else {
+            setResult(eval(screen));
+            setScreen(eval(screen).toString());
+        }
+    }
+
+    const handleDelClick = () => {
+        if(screen.length > 1) {
+            setScreen(screen.substring(0, screen.length - 1));
+        }
+        else {
+            setScreen("0");
+        }
     }
 
   return (
-<>
-    <div className="body">
-
-          <h2 className="screen">{screen}</h2>
-          <div className="section">
-              <button className="num" onClick={handleButttonClick}>1</button>
-              <button className="num" onClick={handleButttonClick}>2</button>
-              <button className="num" onClick={handleButttonClick}>3</button>
-              <button className="operator" onClick={handleButttonClick}>+</button>
+      <>
+          <div className="body">
+              <div className="screensection">
+                  <div className="screenpos">
+                      <h2 className="screen">{screen}</h2>
+                  </div>
+                  <div className="delpos">
+                      <button className="del" onClick={handleDelClick}>{'<'}</button>
+                  </div>
+              </div>
+              <p className="error">{errormsg}</p>
+              <div className="section">
+                  <button className="num" onClick={() => handleButtonClick(1)}>1</button>
+                  <button className="num" onClick={() => handleButtonClick(2)}>2</button>
+                  <button className="num" onClick={() => handleButtonClick(3)}>3</button>
+                  <button className="operator" onClick={() => handleButtonClick("+")}>+</button>
+              </div>
+              <div>
+                  <button className="num" onClick={() => handleButtonClick(4)}>4</button>
+                  <button className="num" onClick={() => handleButtonClick(5)}>5</button>
+                  <button className="num" onClick={() => handleButtonClick(6)}>6</button>
+                  <button className="operator" onClick={() => handleButtonClick("-")}>-</button>
+              </div>
+              <div>
+                  <button className="num" onClick={() => handleButtonClick(7)}>7</button>
+                  <button className="num" onClick={() => handleButtonClick(8)}>8</button>
+                  <button className="num" onClick={() => handleButtonClick(9)}>9</button>
+                  <button className="operator" onClick={() => handleButtonClick("*")}>*</button>
+              </div>
+              <div className="zerosection">
+                  <button className="zer" onClick={() => handleButtonClick(0)}>0</button>
+                  {/*<button className="num" onClick={() => handleButtonClick(".")}>,</button>*/}
+                  <button className="operator" onClick={() => handleCalcClick()}>=</button>
+              </div>
           </div>
-          <div className="section">
-              <button className="num" onClick={handleButttonClick}>4</button>
-              <button className="num" onClick={handleButttonClick}>5</button>
-              <button className="num" onClick={handleButttonClick}>6</button>
-              <button className="operator" onClick={handleButttonClick}>-</button>
-          </div>
-          <div className="section">
-              <button className="num" onClick={handleButttonClick}>7</button>
-              <button className="num" onClick={handleButttonClick}>8</button>
-              <button className="num" onClick={handleButttonClick}>9</button>
-              <button className="operator" onClick={handleButttonClick}>*</button>
-          </div>
-          <div className="section">
-              <button className="zer" onClick={handleButttonClick}>0</button>
-              <button className="num" onClick={handleButttonClick}>,</button>
-              <button className="operator" onClick={handleButttonClick}>=</button>
-          </div>
-      </div>
-</>
+      </>
   );
 }
 
